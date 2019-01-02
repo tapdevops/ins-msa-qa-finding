7266acadcc78c3453f43d99d1da6478bce2c15b1
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-02 16:13:51 +0700

    Ferdinand

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const express = require( 'express' );				
-const mongoose = require( 'mongoose' );				
-const bodyParser = require( 'body-parser' ); 		
-const app = express(); 								
-const config = require( './config/config.js' ); 	
-const dbConfig = require( './config/database.js' ); 
-const uuid = require( 'uuid' );
-const nJwt = require( 'njwt' );
+// Import Express
+const express = require( 'express' );
+
+// Import Mongoose
+const mongoose = require( 'mongoose' );
+
+// Import Body Parser
+const bodyParser = require( 'body-parser' );
+
+// Configuring the Database
+const dbConfig = require( './config/database.js' );
+
+// Configuring configuration
+const config = require( './config/config.js' );
+
+// Define App
+const app = express();
+
+// Parse request of content-type - application/x-www-form-urlencoded
+app.use( bodyParser.urlencoded( { extended: true } ) )
+
+// Parse request of content-type - application/json
+app.use( bodyParser.json() )
 
 // Setup Database
 mongoose.Promise = global.Promise;
@@ -20,23 +35,18 @@ mongoose.connect( dbConfig.url, {
 	console.log( 'Could not connect to the Database. Exiting application.' )
 } );
 
-// Parse request of content-type - application/x-www-form-urlencoded
-app.use( bodyParser.urlencoded( { extended: false } ) );
-
-// Parse request of content-type - application/json
-app.use( bodyParser.json() );
 // Server Running Message
 app.listen( config.app_port, () => {
+	var host = config.ip_address;
+	var port = config.app_port;
 	console.log( config.app_name + ' running on ' + config.app_port )
 } );
+
+// Routes
 app.get( '/', ( req, res ) => {
 	res.json( { 'message': config.app_name } )
 } );
 
-
-
-
-
-
+// Require Bisnis Area Routes
 require( './routes/route.js' )( app );
-
+module.exports = app;
