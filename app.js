df263dfb033f350d5ffa161fe9ae2819d6887d48
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-10 14:54:28 +0700

    Ferdinand

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ mongoose.Promise = global.Promise;
 // Connecting to the database
 mongoose.connect( dbConfig.url, {
 	useNewUrlParser: true,
-	ssl: dbConfig.url
+	ssl: dbConfig.ssl
 } ).then( () => {
 	console.log( 'Successfully connected to the Database' );
 } ).catch( err => {
