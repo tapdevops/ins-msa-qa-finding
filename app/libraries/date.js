5d2fff8dbbbacd01aef31f34b7da19578a70a221
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-14 11:11:21 +0700

    Ferdinand

--- app/libraries/date.js
+++ app/libraries/date.js
@@ -12,17 +12,37 @@ module.exports.convert = function ( value, format ) {
 
 	switch ( format ) {
 		case 'YYYYMMDD':
-			result = value.substr( 0, 4 ) + value.substr( 4, 2 ) + value.substr( 6, 2 );
-			result = value;
+			if ( value.length == 14 || value.length == 8 ) {
+				result = value.substr( 0, 4 ) + value.substr( 4, 2 ) + value.substr( 6, 2 );
+				result = value;
+			}
+			else {
+				result = '';
+			}
 		break;
 		case 'YYYY-MM-DD':
-			result = value.substr( 0, 4 ) + '-' + value.substr( 4, 2 ) + '-' + value.substr( 6, 2 );
+			if ( value.length == 14 || value.length == 8 ) {
+				result = value.substr( 0, 4 ) + '-' + value.substr( 4, 2 ) + '-' + value.substr( 6, 2 );
+			}
+			else {
+				result = '';
+			}
 		break;
 		case 'YYYYMMDDhhmmss':
-			result = value.substr( 0, 4 ) + value.substr( 4, 2 ) + value.substr( 6, 2 ) + value.substr( 8, 2 ) + value.substr( 10, 2 ) + value.substr( 12, 2 );
+			if ( value.length == 14 ) {
+				result = value.substr( 0, 4 ) + value.substr( 4, 2 ) + value.substr( 6, 2 ) + value.substr( 8, 2 ) + value.substr( 10, 2 ) + value.substr( 12, 2 );
+			}
+			else {
+				result = '';
+			}
 		break;
 		case 'YYYY-MM-DD hh-mm-ss':
-			result = value.substr( 0, 4 ) + '-' + value.substr( 4, 2 ) + '-' + value.substr( 6, 2 ) + ' ' + value.substr( 8, 2 ) + ':' + value.substr( 10, 2 ) + ':' + value.substr( 12, 2 );
+			if ( value.length == 14 ) {
+				result = value.substr( 0, 4 ) + '-' + value.substr( 4, 2 ) + '-' + value.substr( 6, 2 ) + ' ' + value.substr( 8, 2 ) + ':' + value.substr( 10, 2 ) + ':' + value.substr( 12, 2 );
+			}
+			else {
+				result = '';
+			}
 		break;
 	}
 
