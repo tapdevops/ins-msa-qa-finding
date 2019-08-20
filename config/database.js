4f7b75d5da534980a7c0f24e137ddf62afac9c59
ferdinand12345 <ferdshinodas@gmail.com>
2019-08-20 11:16:39 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -9,20 +9,6 @@
  |
  */
 	module.exports = {
-		finding: {
-			dev: {
-				url: 'mongodb://s_finding:s_finding@dbappdev.tap-agri.com:4848/s_finding?authSource=s_finding',
-				ssl: false
-			},
-			qa: {
-				url: 'mongodb://s_finding:f1n2019@dbappqa.tap-agri.com:4848/s_finding?authSource=s_finding',
-				ssl: false
-			},
-			prod: {
-				url: 'mongodb://s_finding:f1n2019@dbapp.tap-agri.com:4848/s_finding?authSource=s_finding',
-				ssl: false
-			}
-		},
 		auth:{
 			dev: {
 				url: 'mongodb://s_auth:s_auth@dbappdev.tap-agri.com:4848/s_auth?authSource=s_auth',
@@ -33,7 +19,21 @@
 				ssl: false
 			},
 			prod: {
-				url: 'mongodb://s_auth:4uth2019@dbapp.tap-agri.com:4848/s_auth?authSource=s_auth',
+				url: '',
+				ssl: false
+			}
+		},
+		finding: {
+			dev: {
+				url: 'mongodb://s_finding:s_finding@dbappdev.tap-agri.com:4848/s_finding?authSource=s_finding',
+				ssl: false
+			},
+			qa: {
+				url: 'mongodb://s_finding:f1n2019@dbappqa.tap-agri.com:4848/s_finding?authSource=s_finding',
+				ssl: false
+			},
+			prod: {
+				url: '',
 				ssl: false
 			}
 		},
@@ -47,7 +47,7 @@
 				ssl: false
 			},
 			prod: {
-				url: 'mongodb://s_report:r3p0rt2019@dbapp.tap-agri.com:4848/s_report?authSource=s_report',
+				url: '',
 				ssl: false
 			}
 		}
