3a9a129cf68c20e4afb35efea511002c67f918ae
ihsanhusaeri <ihusaeri@gmail.com>
2019-10-17 14:44:05 +0700

    set time cron untuk generate weekly summary

--- server.js
+++ server.js
@@ -61,7 +61,7 @@
 	} );
 
 	//scheduling job_update_transaksi_complete() with cron 
-	NodeCron.schedule( '* * * * SUN', async () => {
+	NodeCron.schedule( '5 0 * * SUN', async () => {
 		Kernel.job_update_transaksi_complete();
 		console.log( "running node-cron..." );
 	} );
