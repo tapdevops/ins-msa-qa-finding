4c5ca2fbe65d3e337ca88200a00847284f9ece70
ihsanhusaeri <ihusaeri@gmail.com>
2019-10-21 15:17:46 +0700

    tambah or condition di kafka_body

--- app/v1.1/Http/Controllers/ExportKafkaController.js
+++ app/v1.1/Http/Controllers/ExportKafkaController.js
@@ -21,36 +21,36 @@
                 }
             }
         ] );
-
         query.forEach( function( data ) {
             let kafka_body = {
-                FNDCD: data.FINDING_CODE,
-                WERKS: data.WERKS,
-                AFD_CODE: data.AFD_CODE,
-                BLOCK_CODE: data.BLOCK_CODE,
-                FNDCT: data.FINDING_CATEGORY,
-                FNDDS: data.FINDING_DESC,
-                FNDPR: data.FINDING_PRIORITY,
-                DUE_DATE: data.DUE_DATE,
-                ASSTO: data.ASSIGN_TO,
-                PRGRS: data.PROGRESS,
-                LATFN: data.LAT_FINDING,
-                LONFN: data.LONG_FINDING,
-                RFINC: data.REFFERENCE_INS_CODE,
-                INSUR: data.INSERT_USER,
-                INSTM: data.INSERT_TIME,
-                UPTUR: data.UPDATE_USER,
-                UPTTM: data.UPDATE_TIME,
-                DLTUR: data.DELETE_USER,
-                DLTTM: data.DELETE_TIME,
-                RTGVL: data.RATING_VALUE,
-                RTGMS: data.RATING_MESSAGE,
-                END_TIME: data.END_TIME
+                FNDCD: data.FINDING_CODE || "",
+                WERKS: data.WERKS || "",
+                AFD_CODE: data.AFD_CODE || "",
+                BLOCK_CODE: data.BLOCK_CODE || "",
+                FNDCT: data.FINDING_CATEGORY || "",
+                FNDDS: data.FINDING_DES || "",
+                FNDPR: data.FINDING_PRIORITY || "",
+                DUE_DATE: data.DUE_DATE || 0,
+                ASSTO: data.ASSIGN_TO || "",
+                PRGRS: data.PROGRESS || "",
+                LATFN: data.LAT_FINDING || "",
+                LONFN: data.LONG_FINDING || "",
+                RFINC: data.REFFERENCE_INS_CODE || "",
+                INSUR: data.INSERT_USER || "",
+                INSTM: data.INSERT_TIME || 0,
+                UPTUR: data.UPDATE_USER || "",
+                UPTTM: data.UPDATE_TIME || 0,
+                DLTUR: data.DELETE_USER || "",
+                DLTTM: data.DELETE_TIME || 0,
+                RTGVL: data.RATING_VALUE || 0,
+                RTGMS: data.RATING_MESSAGE || "",
+                END_TIME: data.END_TIME || 0
             }
             KafkaServer.producer( 'INS_MSA_FINDING_TR_FINDING', JSON.stringify( kafka_body ) );
         } );
         res.send( {
-            message: true
+            message: true,
+            data: dataSend
         } )
     }
 
