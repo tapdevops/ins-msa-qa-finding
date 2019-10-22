7f1ec226a1321791e53512c084d3b51a0410a401
ihsanhusaeri <ihusaeri@gmail.com>
2019-10-22 16:58:22 +0700

    delete prod database url

--- config/database.js
+++ config/database.js
@@ -19,7 +19,7 @@
 				ssl: false
 			},
 			prod: {
-				url: 'mongodb://s_auth:4uth2019@dbapp.tap-agri.com:4848/s_auth?authSource=s_auth',
+				url: '',
 				ssl: false
 			}
 		},
@@ -47,7 +47,7 @@
 				ssl: false
 			},
 			prod: {
-				url: 'mongodb://s_report:r3p0rt2019@dbapp.tap-agri.com:4848/s_report?authSource=s_report',
+				url: '',
 				ssl: false
 			}
 		}
